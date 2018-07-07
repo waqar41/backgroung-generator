e5ce1ac998f7722ad42f30825fc81666d19d8a8c
hassan <waqar271990@gmail.com>
2018-07-07 18:08:05 +0200

    adding starting project

---
+++ bg1.js
@@ -0,0 +1,13 @@
+ var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body = document.getElementById('gradient');
+
+function changeColor(){
+  body.style.background = "linear-gradient(to right,"
+  + color1.value + "," + color2.value + ")";
+
+}
+
+ color1.addEventListener("input", changeColor);
+
+ color1.addEventListener("input", changeColor);
